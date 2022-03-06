aa242ceffd943201801a4d9d9f825ef6f1dcd5af
Ricsko <szudiricsi@gmail.com>
2022-03-06 20:26:11 +0100

    index.html létrehozása és angularral való összekapcsolása, controllerek létrehozása

--- project/Backend/server.js
+++ project/Backend/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
+const cors = require('cors');
 const mysql = require('mysql');
 const app = express();
 const port = process.env.port || 8080;
+
+
+app.use(express.static(__dirname));
+app.use(express.json());
+app.use(cors());
 
 var kapcsolat = mysql.createConnection({
     host: "localhost",
@@ -21,7 +27,17 @@ kapcsolat.connect((err)=>{
     }
 });
 
-
+app.post('/', (req, res) => {
+    let data = {
+        name: req.body.email,
+        pass: req.body.passwrd,
+        table: req.body.table
+    }
+    pool.query(`SELECT * FROM admin WHERE email='${data.email}' AND jelszo='${data.passwrd}'`, (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
+});
 
 app.listen(port, ()=>{
     console.log(`Server listening on port ${port}...`);
