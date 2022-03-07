7d34abc9f9f873356b76b42e928d0fe8403b3a33
KatonaKoppany <76950463+KatonaKoppany@users.noreply.github.com>
2022-03-07 11:55:54 +0100

    Angular alapok , naptár oldal , TODO

--- project/Frontend/js/app.js
+++ project/Frontend/js/app.js
@@ -1,26 +1,25 @@
 var app = new angular.module('forgalminaplo', ['ngRoute', 'ngAnimate']);
 
-
-app.run(function($rootScope, $locale){
-    $locale.NUMBER_FORMATS.GROUP_SEP = ".";
-    $locale.NUMBER_FORMATS.DECIMAL_SEP = ",";
-    $rootScope.title = "Forgalmi Napló";
-    $rootScope.company = "Bajai SZC Türr István Technikum";
-    $rootScope.author = "KKG SZR LA";
+app.run(function ($rootScope, $locale) {
+    $locale.NUMBER_FORMATS.GROUP_SEP = '.';
+    $locale.NUMBER_FORMATS.DECIMAL_SEP = ',';
+    $rootScope.title = 'Forgalmi Napló';
+    $rootScope.company = 'Bajai SZC Türr István Technikum';
+    $rootScope.author = 'KKG SZR LA';
 
     if (sessionStorage.getItem('forgalminaploUser')) {
         $rootScope.loggedIn = true;
         $rootScope.loggedUser = angular.fromJson(sessionStorage.getItem('forgalminaploUser'));
     } else {
         $rootScope.loggedIn = false;
-        $rootScope.loggedUser = "";
+        $rootScope.loggedUser = '';
     }
 });
 
-app.config(function($routeProvider){
-    $routeProvider
-        .when('/', {
-            templateUrl: 'login.html',
-            controller: 'loginCtrl'
-        })
+//TODO: rooting : Admin - felhasználók + kezelés, Tanár - diákok + naptár(összes foglaáls névvel), Diák - naptár(foglalható órák), saját statisztika
+app.config(function ($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'login.html',
+        controller: 'loginCtrl',
+    });
 });
---
+++ project/Frontend/js/controllers/calendarCtrl.js
@@ -0,0 +1,3 @@
+app.controller('calendarCtrl', function ($scope, $rootScope, factory) {
+    //TODO: Naptár
+});
