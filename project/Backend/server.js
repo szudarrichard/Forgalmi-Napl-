64afb49a268cc0f13c629448f3b69ddf9727f099
Ricsko <szudiricsi@gmail.com>
2022-03-10 20:53:44 +0100

    Adatbázis lekérdezések

--- project/Backend/server.js
+++ project/Backend/server.js
@@ -22,10 +22,87 @@ app.post('/login', (req, res) => {
         table: req.body.table,
     };
 
-    pool.query(`SELECT * FROM ${data.table} WHERE email='${data.email}' AND jelszo='${data.pass}'`, (err, results) => {
+    pool.query(`SELECT * FROM ${data.table} WHERE email='${data.email}' AND password='${data.pass}'`, (err, results) => {
         if (err) throw err;
         res.json(results);
-        console.log(results);
+        
+    });
+});
+
+// GET ALL RECORDS FROM :table
+app.get('/:table', (req, res) => {
+    let table = req.params.table;
+
+    pool.query(`SELECT * FROM ${table}`, (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
+});
+
+// GET ONE RECORD FROM :table
+app.get('/:table/:field/:id', (req, res) => {
+    let table = req.params.table;
+    let field = req.params.field;
+    let id = req.params.id;
+    pool.query(`SELECT * FROM ${table} WHERE ${field}=${id}`, (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
+});
+
+// INSERT RECORD TO :table
+app.post('/:table', (req, res) => {
+    let table = req.params.table;
+    let data = req.body;
+
+    let fields = 'ID';
+    results = Object.keys(data);
+    results.forEach(element => {
+        fields += ',' + element;
+    });
+
+    let str = '';
+    results = Object.values(data);
+    results.forEach(element => {
+        str += ',"' + element + '"';
+    });
+
+    pool.query(`INSERT INTO ${table} (${fields}) VALUES(null ${str})`, (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
+});
+
+// UPDATE RECORD IN :table
+app.patch('/:table/:id', (req, res) => {
+    let table = req.params.table;
+    let id = req.params.id;
+    let data = req.body;
+
+
+    let str = '';
+    fields = Object.keys(data);
+    results = Object.values(data);
+
+    for (let i = 0; i < fields.length; i++) {
+        str += fields[i] + '="' + results[i] + '",';
+    }
+
+    str = str.substring(0, str.length - 1);
+
+    pool.query(`UPDATE ${table} SET ${str} WHERE ID=${id}`, (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
+});
+
+// DELETE ONE RECORD FROM :table
+app.delete('/:table/:id', (req, res) => {
+    let table = req.params.table;
+    let id = req.params.id;
+    pool.query(`DELETE FROM ${table} WHERE ID=${id}`, (err, results) => {
+        if (err) throw err;
+        res.json(results);
     });
 });
 
