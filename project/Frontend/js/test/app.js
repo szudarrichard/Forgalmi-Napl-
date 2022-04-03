8ad40a470763037820cd053c76e7826e1e196cf6
Ricsko <szudiricsi@gmail.com>
2022-04-03 20:15:07 +0200

    random jelszó generálás kezdetleges verzió

---
+++ project/Frontend/js/test/app.js
@@ -0,0 +1,13 @@
+let password = document.getElementById("password");
+
+ function genPassword() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 8;
+    var password = "";
+    for (var i = 0; i <= passwordLength; i++) 
+    {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
+    }
+    document.getElementById("password").value = password;
+}
