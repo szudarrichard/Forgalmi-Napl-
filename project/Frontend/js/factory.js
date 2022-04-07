a8a59028b752fc87f42059e8d45c78cdb430b618
KatonaKoppany <76950463+KatonaKoppany@users.noreply.github.com>
2022-04-07 02:02:22 +0200

    A felhasználó 1 nap csak 1 órát tud foglalni A controllerből át adott adatok felhasználása

--- project/Frontend/js/factory.js
+++ project/Frontend/js/factory.js
@@ -128,25 +128,42 @@ app.factory('factory', function ($http, $q) {
                 buttonIcons: false, // show the prev/next text
                 weekNumbers: true,
                 navLinks: true, // can click day/week names to navigate views
-                editable: edit,
                 dayMaxEvents: true, // allow \"more\" link when too many events
-                eventColor: '#C70039',
+                eventColor: events.color,
+                eventResizableFromStart: false,
                 height: 750,
                 select: function (arg) {
+                    //TODO console-log helyére be kell húzni a factory alert function-t
                     $http.get(url + '/' + 'student' + '/' + 'email' + '/' + angular.fromJson(sessionStorage.getItem('email'))).then(function (res) {
                         let data = {
                             start: moment(arg.start).locale('hu').format('YYYY-MM-DD HH:mm'),
-                            end: moment(arg.end).locale('hu').format('YYYY-MM-DD HH:mm'),
+                            end: moment(arg.start).add(1, 'hours').locale('hu').format('YYYY-MM-DD HH:mm'), //TODO azonnal frissíteni kell az oldalt!!! VAGY keresni full calendar hove eseményt
                             studentID: res.data[0].ID,
                         };
 
-                        $http.post(url + '/' + tablename, data).then(function (res) {
-                            calendar.addEvent({
-                                start: arg.start,
-                                end: arg.end,
-                                id: res.data.insertId,
-                                allDay: arg.allDay,
-                            });
+                        $http.get(url + '/' + tablename).then(function (res) {
+                            for (let i = 0; i < res.data.length; i++) {
+                                if (data.studentID != res.data[i].studentID) {
+                                } else {
+                                    if (data.start.substring(0, 10) != res.data[i].start.substring(0, 10)) {
+                                    } else {
+                                        data.start = '';
+                                        console.log('Már van foglat órád ezen a napon');
+                                    }
+                                }
+                            }
+
+                            if (data.start.length != 0) {
+                                console.log('Óra sikeresen rögzítve!');
+                                $http.post(url + '/' + tablename, data).then(function (res) {
+                                    calendar.addEvent({
+                                        start: arg.start,
+                                        end: arg.end,
+                                        id: res.data.insertId,
+                                        allDay: arg.allDay,
+                                    });
+                                });
+                            }
                         });
 
                         calendar.unselect();
@@ -157,9 +174,10 @@ app.factory('factory', function ($http, $q) {
                     $http.get(url + '/' + tablename + '/' + 'ID' + '/' + arg.event.id).then(function (res) {
                         $http.get(url + '/' + 'student' + '/' + 'ID' + '/' + res.data[0].studentID).then(function (res) {
                             if (res.data[0].email != angular.fromJson(sessionStorage.getItem('email'))) {
-                                //TODO: modál az adtokkal
+                                console.log('Ez az óra már foglalt!');
                             } else {
                                 $http.delete(url + '/' + tablename + '/' + arg.event.id).then(function (res) {
+                                    console.log('Óra eltávolítva!');
                                     arg.event.remove();
                                 });
                             }
@@ -180,7 +198,6 @@ app.factory('factory', function ($http, $q) {
 
         //ALERT WINDOW
         alert: function (message, type, icon) {
-            console.log(message, type, icon);
             var alertplaceholder = document.getElementById('alertplaceholder');
             var wrapper = document.createElement('div');
             wrapper.innerHTML =
